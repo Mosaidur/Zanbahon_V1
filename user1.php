f832ffbcc8ad0de79e7bdeeba249a8b3b394a94c
Mosaidur <asifbhuiyan135@gmail.com>
2024-11-30 21:49:25 +0600

    user1 "mothod not found" solved

--- user1.php
+++ user1.php
@@ -3,16 +3,12 @@
 
 <?php
 header("Content-Type: application/json");
-require 'Connection.php';
-
-if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-    // Handle preflight request
-    header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Origin: *");
     header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
     header("Access-Control-Allow-Headers: Content-Type, Authorization");
-    http_response_code(200);
-    exit;
-};
+require 'Connection.php';
+
+$method = $_SERVER['REQUEST_METHOD'];
 
 try {
     switch ($method) {
